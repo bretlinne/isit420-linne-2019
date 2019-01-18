df14c4000f781901c114f86b52864bddd62f9534
bret isit420 <bretlinne@gmail.com>
2019-01-17 18:04:21 -0800

    finished ass1

--- week01_create450/routes/index.js
+++ week01_create450/routes/index.js
@@ -13,4 +13,15 @@ router.get('/', function(req, res){
   res.sendFile(path.join(__dirname+'.index.html'));
 });
 */
+
+/* GET Userlist page */
+router.get('/userlist', function(req,res){
+  var db = req db;
+  var collection = db.get('Usercollection');
+  collection.find({},{},function(e,docs){
+    res.render('userlist', {
+      "userlist" : docs
+    });
+  });
+});
 module.exports = router;
